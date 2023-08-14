88769539e2fe78b860f75f71e4b8fcde7926638c
AmnaZa <113046745+AmnaZa@users.noreply.github.com>
2023-08-14 11:15:36 -0400

    Magic 8 ball

--- server.js
+++ server.js
@@ -20,6 +20,26 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`Your tip will be: ${tipAmount}`);
   });
 
+
+  // Magic 8 Ball responses array
+const magic8BallResponses = [
+    "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+    "You may rely on it", "As I see it yes", "Most likely", "Outlook good",
+    "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
+    "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+    "Don't count on it", "My reply is no", "My sources say no",
+    "Outlook not so good", "Very doubtful"
+  ];
+  
+  // Magic 8 Ball route
+  app.get('/magic/:question', (req, res) => {
+    const question = req.params.question.replace(/%20/g, ' ');
+    const randomResponse = magic8BallResponses[Math.floor(Math.random() * magic8BallResponses.length)];
+    res.send(`<h1>Question: ${question}</h1><h1>Response: ${randomResponse}</h1>`);
+  });
+
+
+  
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
