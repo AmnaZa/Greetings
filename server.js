5d6272beb73cc9ff845c390f846e639df0dafea0
AmnaZa <113046745+AmnaZa@users.noreply.github.com>
2023-08-14 11:08:42 -0400

    tip Calculator

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.get('/greeting/:name', (req, res) => {
   res.send(greeting);
 });
 
+// Tip Calculator route
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const total = (req.params.total);
+    const tipPercentage = (req.params.tipPercentage);
+    const tipAmount = (total * tipPercentage) / 100;
+    res.send(`Your tip will be: ${tipAmount}`);
+  });
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
